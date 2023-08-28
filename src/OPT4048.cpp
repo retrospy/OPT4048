36400800029c0c28fcdd17f5839fb834d6451240
Christopher J Mallery <zoggins@retro-spy.com>
2023-08-27 18:42:01 -0700

    Version 1.1 Update

--- src/OPT4048.cpp
+++ src/OPT4048.cpp
@@ -33,11 +33,11 @@ THE SOFTWARE.
 
 OPT4048::OPT4048()
 {
+	ADC_to_XYZ[0][0] = 2.34892992e-4;
 }
 
 OPT4048_ErrorCode OPT4048::begin(uint8_t address) 
 {
-	OPT4048_ErrorCode error = NO_ERROR;
 	_address = address;
 	Wire.begin();
 
@@ -53,25 +53,23 @@ uint16_t OPT4048::readDeviceID()
 	return result;
 }
 
-OPT4048_ConfigA OPT4048::readConfigA() 
+OPT4048_ErrorCode OPT4048::readConfig(OPT4048_ConfigA& config)
 {
-	OPT4048_ConfigA config;
 	OPT4048_ErrorCode error = writeData(CONFIG_A);
 	if (error == NO_ERROR)
 		error = readData(&config.rawData);
-	return config;
+	return error;
 }
 
-OPT4048_ConfigB OPT4048::readConfigB() 
+OPT4048_ErrorCode OPT4048::readConfig(OPT4048_ConfigB& config)
 {
-	OPT4048_ConfigB config;
 	OPT4048_ErrorCode error = writeData(CONFIG_B);
 	if (error == NO_ERROR)
 		error = readData(&config.rawData);
-	return config;
+	return error;
 }
 
-OPT4048_ErrorCode OPT4048::writeConfigA(OPT4048_ConfigA config) 
+OPT4048_ErrorCode OPT4048::writeConfig(OPT4048_ConfigA config) 
 {
 	config.FixedZero = 0;
 	Wire.beginTransmission(_address);
@@ -81,7 +79,7 @@ OPT4048_ErrorCode OPT4048::writeConfigA(OPT4048_ConfigA config)
 	return (OPT4048_ErrorCode)(-10 * Wire.endTransmission());
 }
 
-OPT4048_ErrorCode OPT4048::writeConfigB(OPT4048_ConfigB config) 
+OPT4048_ErrorCode OPT4048::writeConfig(OPT4048_ConfigB config) 
 {
 	config.FixedZero = 0;
 	config.Fixed128 = 128;
@@ -92,59 +90,69 @@ OPT4048_ErrorCode OPT4048::writeConfigB(OPT4048_ConfigB config)
 	return (OPT4048_ErrorCode)(-10 * Wire.endTransmission());
 }
 
-OPT4048_RESULT OPT4048::readResult(OPT4048_Channel channel) 
+OPT4048_THRESHOLD OPT4048::readLimit(OPT4048_Commands command)
 {
-	OPT4048_REGISTER registerResponse = readRegister((OPT4048_Commands)channel);
-	if (registerResponse.error == NO_ERROR) 
+	OPT4048_THRESHOLD limit;
+	limit.error = writeData(command);
+	if (limit.error == NO_ERROR)
+		limit.error = readData(&limit.rawResult.rawData);
+	return limit;
+}
+
+OPT4048_ErrorCode OPT4048::writeLimit(OPT4048_Commands command, OPT4048_ER12 threshold)
+{
+	Wire.beginTransmission(_address);
+	Wire.write(command);
+	Wire.write(threshold.rawData >> 8);
+	Wire.write(threshold.rawData & 0x00FF);
+	return (OPT4048_ErrorCode)(-10 * Wire.endTransmission());
+}
+
+OPT4048_RESULT OPT4048::readChannel(OPT4048_Channel channel) 
+{
+	OPT4048_THRESHOLD register1 = readLimit((OPT4048_Commands)channel);
+	if (register1.error == NO_ERROR)
 	{
-		uint8_t exponent = registerResponse.value >> 12;
-		uint32_t mantissa = (uint32_t)registerResponse.value << 8;
-		registerResponse = readRegister((OPT4048_Commands)(channel+1));
-		if (registerResponse.error == NO_ERROR) 
+		uint8_t exponent = register1.rawResult.Exponent;
+		uint32_t mantissa = register1.rawResult.Mantissa << 8;
+		OPT4048_REGISTER register2 = readRegister((OPT4048_Commands)(channel + 1));
+		if (register2.error == NO_ERROR)
 		{
 			OPT4048_RESULT result;
-			result.rawResult.Mantissa = mantissa | registerResponse.value >> 8;
+			result.rawResult.Mantissa = mantissa + (register2.value >> 8);
 			result.rawResult.Exponent = exponent;
 			result.error = NO_ERROR;
 			return result;
-		}	
-		else 
-		{
-			return returnResultError(registerResponse.error);
 		}
-	}	
-	else 
+		else
+		{
+			return returnResultError(register2.error);
+		}
+	}
+	else
 	{
-		return returnResultError(registerResponse.error);
+		return returnResultError(register1.error);
 	}
 }
 
 OPT4048_THRESHOLD OPT4048::readHighLimit() 
 {
-	return readThresholdRegister(THRESHOLD_HIGH);
+	return readLimit(THRESHOLD_HIGH);
 }
 
 OPT4048_ErrorCode OPT4048::writeHighLimit(OPT4048_ER12 threshold)
 {
-	Wire.beginTransmission(_address);
-	Wire.write(THRESHOLD_HIGH);
-	Wire.write(threshold.rawData >> 8);
-	Wire.write(threshold.rawData & 0x00FF);
-	return (OPT4048_ErrorCode)(-10 * Wire.endTransmission());	
+	return writeLimit(THRESHOLD_HIGH, threshold);
 }
 
 OPT4048_THRESHOLD OPT4048::readLowLimit() 
 {
-	return readThresholdRegister(THRESHOLD_LOW);
+	return readLimit(THRESHOLD_LOW);
 }
 
 OPT4048_ErrorCode OPT4048::writeLowLimit(OPT4048_ER12 threshold)
 {
-	Wire.beginTransmission(_address);
-	Wire.write(THRESHOLD_LOW);
-	Wire.write(threshold.rawData >> 8);
-	Wire.write(threshold.rawData & 0x00FF);
-	return (OPT4048_ErrorCode)(-10 * Wire.endTransmission());
+	return writeLimit(THRESHOLD_LOW, threshold);
 }
 
 OPT4048_REGISTER OPT4048::readRegister(OPT4048_Commands command) 
@@ -153,50 +161,94 @@ OPT4048_REGISTER OPT4048::readRegister(OPT4048_Commands command)
 	result.error = writeData(command);
 	if (result.error == NO_ERROR) 
 	{
-
 		result.error = readData(&result.value);
-
-		return result;
 	}
-	else 
-	{
-		return result;
-	}
+	return result;
 }
-
-OPT4048_THRESHOLD OPT4048::readThresholdRegister(OPT4048_Commands command) 
-{
-	OPT4048_ErrorCode error = writeData(command);
-	if (error == NO_ERROR) 
-	{
-		OPT4048_THRESHOLD result;
-		result.error = NO_ERROR;
-
-		OPT4048_ER12 er;
-		error = readData(&er.rawData);
-		if (error == NO_ERROR) 
-		{
-			result.rawResult = er;
-		}
-		else 
-		{
-			result.error = error;
-		}
-
-		return result;
-	}
-	else 
-	{	
-		return returnThresholdError(error);
-	}
-}
-
 
 OPT4048_ErrorCode OPT4048::writeData(OPT4048_Commands command)
 {
 	Wire.beginTransmission(_address);
 	Wire.write(command);
 	return (OPT4048_ErrorCode)(-10 * Wire.endTransmission(true));
+}
+
+OPT4048_ErrorCode OPT4048::readADC(OPT4048_ADC& values)
+{
+	OPT4048_RESULT result = readChannel(CHANNEL_0);
+	if (result.error == NO_ERROR)
+	{
+		values.channel_0 = result.GetADCValue();
+		result = readChannel(CHANNEL_1);
+		if (result.error == NO_ERROR)
+		{
+			values.channel_1 = result.GetADCValue();
+			result = readChannel(CHANNEL_2);
+			if (result.error == NO_ERROR)
+			{
+				values.channel_2 = result.GetADCValue();
+				result = readChannel(CHANNEL_3);
+				if (result.error == NO_ERROR)
+				{
+					values.channel_3 = result.GetADCValue();
+				}
+			}
+		}
+	}
+
+	return result.error;
+}
+
+OPT4048_ErrorCode OPT4048::readXYZ(OPT4048_XYZ& values)
+{
+	// This is from the datasheet
+	float ADC_to_XYZ[4][4] = { { 2.34892992e-4, -1.89652390e-5,  1.20811684e-5,       0 },
+							   { 4.07467441e-5,  1.98958202e-4, -1.58848115e-5, 2.15e-3 },
+							   { 9.28619404e-5, -1.69739553e-5,  6.74021520e-4,       0 },
+							   {             0,              0,              0,       0 }
+	};
+	OPT4048_ADC adc;
+	OPT4048_ErrorCode err = readADC(adc);
+
+	if (err == NO_ERROR)
+	{
+		values.X   = adc.channel_0 * ADC_to_XYZ[0][0] + adc.channel_1 * ADC_to_XYZ[1][0] + adc.channel_2 * ADC_to_XYZ[2][0] + adc.channel_3 * ADC_to_XYZ[3][0];
+		values.Y   = adc.channel_0 * ADC_to_XYZ[0][1] + adc.channel_1 * ADC_to_XYZ[1][1] + adc.channel_2 * ADC_to_XYZ[2][1] + adc.channel_3 * ADC_to_XYZ[3][1];
+		values.Z   = adc.channel_0 * ADC_to_XYZ[0][2] + adc.channel_1 * ADC_to_XYZ[1][2] + adc.channel_2 * ADC_to_XYZ[2][2] + adc.channel_3 * ADC_to_XYZ[3][2];
+		values.Lux = adc.channel_0 * ADC_to_XYZ[0][3] + adc.channel_1 * ADC_to_XYZ[1][3] + adc.channel_2 * ADC_to_XYZ[2][3] + adc.channel_3 * ADC_to_XYZ[3][3];
+
+	}
+
+	return err;
+}
+
+OPT4048_ErrorCode OPT4048::readCIE(OPT4048_CIE& values)
+{
+	OPT4048_XYZ result;
+
+	OPT4048_ErrorCode err = readXYZ(result);
+
+	if (err == NO_ERROR)
+	{
+		float denominator = result.X + result.Y + result.Z;
+		values.X = result.X / denominator;
+		values.Y = result.Y / denominator;
+	}
+
+	return err;
+}
+
+OPT4048_ErrorCode OPT4048::readLux(float& lux)
+{
+	OPT4048_RESULT result = readChannel(CHANNEL_1);
+
+	if (result.error == NO_ERROR)
+	{
+		lux = result.GetADCValue() * 2.15e-3;
+	}
+
+	return result.error;
+
 }
 
 OPT4048_ErrorCode OPT4048::readData(uint16_t* data)
--- src/OPT4048.h
+++ src/OPT4048.h
@@ -33,7 +33,8 @@ THE SOFTWARE.
 
 #include <Arduino.h>
 
-typedef enum {
+enum OPT4048_Commands  
+{
 	RESULT_CH0_1		= 0x00,
 	RESULT_CH0_2		= 0x01,
 	RESULT_CH1_1		= 0x02,
@@ -47,10 +48,10 @@ typedef enum {
 	CONFIG_A			= 0x0A,
 	CONFIG_B			= 0x0B,
 	DEVICE_ID			= 0x11,
-} OPT4048_Commands;
+};
 
-
-typedef enum {
+ enum OPT4048_ErrorCode 
+ {
 	NO_ERROR = 0,
 	TIMEOUT_ERROR = -100,
 
@@ -59,71 +60,118 @@ typedef enum {
 	WIRE_I2C_RECEIVED_NACK_ON_ADDRESS = -20,
 	WIRE_I2C_RECEIVED_NACK_ON_DATA = -30,
 	WIRE_I2C_UNKNOW_ERROR = -40
-} OPT4048_ErrorCode;
+} ;
 
-typedef enum {
+enum OPT4048_Channel 
+{
 	CHANNEL_0 = 0x00,
 	CHANNEL_1 = 0x02,
 	CHANNEL_2 = 0x04,
 	CHANNEL_3 = 0x06
-} OPT4048_Channel;
+};
 
-typedef union {
+union OPT4048_ConfigA
+{
 	struct __attribute__((__packed__)) {
-		uint8_t FaultCount : 2;
-		uint8_t Polarity : 1;
-		uint8_t Latch : 1;
-		uint8_t OpMode : 2;
-		uint8_t ConversionTime : 4;
-		uint8_t RangeNumber : 4;
-		uint8_t FixedZero : 1;
-		uint8_t QuickWake : 1;
+		uint8_t FaultCount		: 2;
+		uint8_t Polarity		: 1;
+		uint8_t Latch			: 1;
+		uint8_t OpMode			: 2;
+		uint8_t ConversionTime  : 4;
+		uint8_t RangeNumber		: 4;
+		uint8_t FixedZero		: 1;
+		uint8_t QuickWake		: 1;
 	};
 	uint16_t rawData;
-} OPT4048_ConfigA;
 
-typedef union {
-	struct __attribute__((__packed__)) {
-		uint8_t I2CBurst : 1;
-		uint8_t FixedZero : 1;
-		uint8_t IntConfig : 2;
-		uint8_t IntDirection : 1;
+	OPT4048_ConfigA()
+	{
+		rawData = 0x3208;
+	}
+};
+
+ union OPT4048_ConfigB
+ {
+	struct __attribute__((__packed__))
+	{
+		uint8_t I2CBurst		 : 1;
+		uint8_t FixedZero		 : 1;
+		uint8_t IntConfig		 : 2;
+		uint8_t IntDirection	 : 1;
 		uint8_t ThresholdChannel : 2;
-		uint8_t RangeNumber : 4;
-		uint16_t Fixed128 : 9;
+		uint8_t RangeNumber		 : 4;
+		uint16_t Fixed128		 : 9;
 	};
 	uint16_t rawData;
-} OPT4048_ConfigB;
 
-typedef union {
-	uint16_t rawData;
-	struct __attribute__((__packed__)) {
-		uint32_t Mantissa : 20;
-		uint8_t Exponent : 4;
-	};
-} OPT4048_ER20;
+	OPT4048_ConfigB()
+	{
+		rawData = 0x8011;
+	}
+};
 
-typedef union {
-	uint16_t rawData;
+ struct OPT4048_ER20
+ {
+	 uint32_t Mantissa : 20;
+	 uint8_t Exponent : 4;
+ };
+
+
+union OPT4048_ER12 
+{
 	struct __attribute__((__packed__)) {
 		uint32_t Mantissa : 12;
 		uint8_t Exponent : 4;
 	};
-} OPT4048_ER12;
+	uint16_t rawData;
+};
 
-struct OPT4048_RESULT {
+struct OPT4048_RESULT 
+{
 	OPT4048_ER20 rawResult;
 	OPT4048_ErrorCode error;
+
+	float GetADCValue()
+	{
+		return rawResult.Mantissa << rawResult.Exponent;
+	}
 };
 
 struct OPT4048_THRESHOLD {
 	OPT4048_ER12 rawResult;
 	OPT4048_ErrorCode error;
+
+	float GetADCValue()
+	{
+		return rawResult.Mantissa << rawResult.Exponent;
+	}
 };
 
 struct OPT4048_REGISTER {
 	uint16_t value;
 	OPT4048_ErrorCode error;
+};
+
+struct OPT4048_ADC
+{
+	float channel_0;
+	float channel_1;
+	float channel_2;
+	float channel_3;
+};
+
+struct OPT4048_XYZ
+{
+	float X;
+	float Y;
+	float Z;
+	float Lux;
+};
+
+struct OPT4048_CIE
+{
+	float X;
+	float Y;
 };
 
 class OPT4048 {
@@ -134,17 +182,24 @@ public:
 
 	uint16_t readDeviceID();
 
-	OPT4048_RESULT readResult(OPT4048_Channel channel);
-	OPT4048_THRESHOLD readHighLimit();
-	OPT4048_ErrorCode writeHighLimit(OPT4048_ER12 threshold);
-	OPT4048_THRESHOLD readLowLimit();
-	OPT4048_ErrorCode writeLowLimit(OPT4048_ER12 threshold);
-	
-	OPT4048_ConfigA readConfigA();
-	OPT4048_ErrorCode writeConfigA(OPT4048_ConfigA config);
+	OPT4048_ErrorCode readADC(OPT4048_ADC& values);
+	OPT4048_ErrorCode readXYZ(OPT4048_XYZ& values);
+	OPT4048_ErrorCode readCIE(OPT4048_CIE& values);
+	OPT4048_ErrorCode readLux(float& lux);
 
-	OPT4048_ConfigB readConfigB();
-	OPT4048_ErrorCode writeConfigB(OPT4048_ConfigB config);
+	OPT4048_RESULT readChannel(OPT4048_Channel channel);
+
+	OPT4048_THRESHOLD readLowLimit();
+	OPT4048_THRESHOLD readHighLimit();
+
+	OPT4048_ErrorCode writeLowLimit(OPT4048_ER12 threshold);
+	OPT4048_ErrorCode writeHighLimit(OPT4048_ER12 threshold);
+
+	OPT4048_ErrorCode readConfig(OPT4048_ConfigA& config);
+	OPT4048_ErrorCode readConfig(OPT4048_ConfigB& config);
+
+	OPT4048_ErrorCode writeConfig(OPT4048_ConfigA config);
+	OPT4048_ErrorCode writeConfig(OPT4048_ConfigB config);
 
 private:
 	uint8_t _address;
@@ -152,10 +207,18 @@ private:
 	OPT4048_ErrorCode writeData(OPT4048_Commands command);
 	OPT4048_ErrorCode readData(uint16_t* data);
 
+	OPT4048_THRESHOLD readLimit(OPT4048_Commands command);
+	OPT4048_ErrorCode writeLimit(OPT4048_Commands command, OPT4048_ER12 threshold);
+
 	OPT4048_REGISTER readRegister(OPT4048_Commands command);
-	OPT4048_THRESHOLD readThresholdRegister(OPT4048_Commands command);
 	OPT4048_RESULT returnResultError(OPT4048_ErrorCode error);
 	OPT4048_THRESHOLD returnThresholdError(OPT4048_ErrorCode error);
+
+	float ADC_to_XYZ[4][4] = { { 2.34892992e-4, -1.89652390e-5,  1.20811684e-5,       0 },
+							          { 4.07467441e-5,  1.98958202e-4, -1.58848115e-5, 2.15e-3 },
+		                              { 9.28619404e-5, -1.69739553e-5,  6.74021520e-4,       0 },
+							          {             0,              0,              0,       0 }
+	};
 
 };
 
